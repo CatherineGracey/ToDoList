80745df03126ceb18df8bff42e16f289d7f6f94a
Catherine Gracey <catherine.gracey@gmail.com>
2016-10-10 12:43:48 +1100

    Moves items between lists

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var addBtn = document.getElementById("add-btn");
-var newToDoInput = document.getElementById("new-todo-input");
-var toDoList = document.getElementById("todo-list");
+console.log("This is the app.js file");
+
+var addBtn = document.querySelector("#add-btn");
+var newToDoInput = document.querySelector("#new-todo-input");
+var toDoList = document.querySelector("#todo-list");
 
 var addToDo = function(){
   var todoContent = newToDoInput.value;
@@ -8,6 +10,24 @@ var addToDo = function(){
   var newContent = document.createTextNode(todoContent);
   newLI.appendChild(newContent);
   toDoList.appendChild(newLI);
+  newToDoInput.value = "";
 };
 
 addBtn.addEventListener("click", addToDo);
+
+document.getElementById("lists").addEventListener("click", function(event){
+  console.log("someone clicked on you or on your children");
+  console.log(event.target);
+  //console.log(this);//Shows the element the listener is attached to, not what is clicked directly
+  if (event.target.tagName === "LI"){
+    var toDo = event.target;
+    toDo.parentNode.removeChild(toDo);
+    if (event.target.className === "done"){
+      toDo.className = "";
+      toDoList.appendChild(toDo);
+    } else {
+      toDo.className = "done";
+      document.getElementById("done-list").appendChild(toDo);
+    }
+  }
+});
